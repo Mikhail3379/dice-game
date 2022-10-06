bdba2eba55754ad674d0d7a9cf88032f5bfef315
Mikhail Bakushev <mikhail.bakushev@outlook.com>
2022-10-06 16:19:46 +0100

    Delete module.js

--- Dice game/module.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-var player = new Schema({
-name: String,
-score: Number
-
- })
-module.exports = mongoose.model("player", playerSchema)
