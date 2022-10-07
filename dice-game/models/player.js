ce1f1f99942ddfb8391ab81fae68656c5da68856
Mikhail Bakushev <mikhail.bakushev@outlook.com>
2022-10-08 00:02:11 +0100

    Update player.js

--- dice-game/models/player.js
+++ dice-game/models/player.js
@@ -3,8 +3,10 @@ const schema = mongoose.Schema
 
 const playerSchema = new schema({
   
-    name: { type: String, required: true },
-    score: { type: Number, required: true },
+    name1: { type: String, required: true },
+    score1: { type: Number, required: true },
+    name2: { type: String, required: true },
+    score2: { type: Number, required: true }
   
 });
 
